2382849eafc8df152f726d2d9061a5849e9eb32f
R1nge <r1ngegamedev@gmail.com>
2025-03-04 00:50:38 +0300

    loop through all oga files

--- detect.py
+++ detect.py
@@ -1,24 +1,20 @@
 from deep_emotion_recognition import DeepEmotionRecognizer
 import json
-# initialize instance
-# inherited from emotion_recognition.EmotionRecognizer
+import os
 # default parameters (LSTM: 128x2, Dense:128x2)
 deeprec = DeepEmotionRecognizer(emotions=['angry', 'sad', 'neutral', 'ps', 'happy'], n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128)
-# train the model
 deeprec.train()
-# get the accuracy
 print(deeprec.test_score())
-# predict angry audio sample
-
-# I have 2 options, either run it every time I need
-# OR periodically check for files
 
 
 sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
-prediction = deeprec.predict(f"{sharedPath}/1.oga")
-print(f"Prediction: {prediction}")
-data = {
-    "emotion": prediction
-}
-with open(f'{sharedPath}/emotions.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+for file in os.listdir(sharedPath):
+    if file.endswith(".oga"):
+        # Analyze each file and save to a JSON
+        prediction = deeprec.predict(os.path.join(sharedPath, file))
+        print(f"Prediction for {file}: {prediction}")
+        data = {
+            "emotion": prediction
+        }
+        with open(os.path.join(sharedPath, f"{file}.json"), 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)

5f83e592f5e784c5477880b64ac7b6d089559c75
R1nge <r1ngegamedev@gmail.com>
2025-03-05 01:07:45 +0300

    launch once and wait for state

--- detect.py
+++ detect.py
@@ -1,38 +1,50 @@
 from deep_emotion_recognition import DeepEmotionRecognizer
 import json
 import os
+import time
 
 sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
 statePath = os.path.join(sharedPath, "STATE")
 statePathJson = os.path.join(statePath, "STATE.json")
-
-state = {
-    "state": "detection_started"
-}
-with open(statePathJson,'w', encoding='utf-8') as f:
-    json.dump(state, f ,ensure_ascii=False, indent=4)
-
 
 # default parameters (LSTM: 128x2, Dense:128x2)
 deeprec = DeepEmotionRecognizer(emotions=['angry', 'sad', 'neutral', 'ps', 'happy'], n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128)
 deeprec.train()
 print(deeprec.test_score())
 
-i = 0
-for file in os.listdir(sharedPath):
-    if file.endswith(".oga"):
-        # Analyze each file and save to a JSON
-        prediction = deeprec.predict(os.path.join(sharedPath, file))
-        print(f"Prediction for {file}: {prediction}")
-        data = {
-            "emotion": prediction
-        }
-        with open(os.path.join(sharedPath, f"{i}.json"), 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-        i += 1
-
 state = {
-    "state": "detection_finished"
-}
-with open(statePathJson,'w', encoding='utf-8') as f:
-    json.dump(state, f,ensure_ascii=False, indent=4)
+            "state": "detection_started"
+        }
+
+while True:
+    with open(statePathJson, 'r', encoding='utf-8') as f:
+        contents = f.read()
+        state = json.loads(contents)
+        print(state)
+
+    if state['state'] == "splitting_finished":        
+        with open(statePathJson,'w', encoding='utf-8') as f:
+            json.dump(state, f ,ensure_ascii=False, indent=4)
+
+        i = 0
+        for file in os.listdir(sharedPath):
+            if file.endswith(".oga"):
+                # Analyze each file and save to a JSON
+                prediction = deeprec.predict(os.path.join(sharedPath, file))
+                print(f"Prediction for {file}: {prediction}")
+                data = {
+                    "emotion": prediction
+                }
+                with open(os.path.join(sharedPath, f"{i}.json"), 'w', encoding='utf-8') as f:
+                    json.dump(data, f, ensure_ascii=False, indent=4)
+                i += 1
+
+        state = {
+            "state": "detection_finished"
+        }
+        with open(statePathJson,'w', encoding='utf-8') as f:
+            json.dump(state, f,ensure_ascii=False, indent=4)
+        time.sleep(10)
+    else:
+        print("waiting")
+        time.sleep(1)

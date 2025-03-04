bdc3c730c5da89a8ee85523ab87202b434d19fbb
R1nge <r1ngegamedev@gmail.com>
2025-03-04 19:07:16 +0300

    set and save state

--- detect.py
+++ detect.py
@@ -1,13 +1,23 @@
 from deep_emotion_recognition import DeepEmotionRecognizer
 import json
 import os
+
+sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
+statePath = os.path.join(sharedPath, "STATE")
+statePathJson = os.path.join(statePath, "STATE.json")
+
+state = {
+    "state": "detection_started"
+}
+with open(statePathJson,'w', encoding='utf-8') as f:
+    json.dump(state, f ,ensure_ascii=False, indent=4)
+
+
 # default parameters (LSTM: 128x2, Dense:128x2)
 deeprec = DeepEmotionRecognizer(emotions=['angry', 'sad', 'neutral', 'ps', 'happy'], n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128)
 deeprec.train()
 print(deeprec.test_score())
 
-
-sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
 i = 0
 for file in os.listdir(sharedPath):
     if file.endswith(".oga"):
@@ -20,3 +30,9 @@ for file in os.listdir(sharedPath):
         with open(os.path.join(sharedPath, f"{i}.json"), 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         i += 1
+
+state = {
+    "state": "detection_finished"
+}
+with open(statePathJson,'w', encoding='utf-8') as f:
+    json.dump(state, f,ensure_ascii=False, indent=4)

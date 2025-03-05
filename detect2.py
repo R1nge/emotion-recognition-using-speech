40563d5411c23a0be57407a6e3ce1fafc5d722c3
R1nge <r1ngegamedev@gmail.com>
2025-03-05 23:44:22 +0300

    analyze face emotion

--- detect2.py
+++ detect2.py
@@ -1,4 +1,6 @@
 from deepface import DeepFace
+import json
+import os
 
 backends = [
   'opencv', 
@@ -16,14 +18,28 @@ backends = [
   'centerface',
 ]
 
-alignment_modes = [True, False]
+sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
 
 #split video into images, 1 image per 950ms
 #analyze each image
 #save to a file
-objs = DeepFace.analyze(
-  img_path = "img.jpg", 
-  actions = ['age', 'gender', 'race', 'emotion']
-)
+
+i = 0
+for file in os.listdir(sharedPath):
+    if file.endswith(".mp4"):
+        # Analyze each file and save to a JSON
+        objs = DeepFace.analyze(
+            img_path = "img.jpg", 
+            actions = ['age', 'gender', 'race', 'emotion']
+        )
+        print(f"Prediction for {file}: {objs}")
+        data = objs[0]['dominant_emotion']
+        print(data)
+        with open(os.path.join(sharedPath, f"{i}_face.json"), 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        i += 1
+
+
+
 
 print(objs)

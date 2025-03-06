a43a21b878fa46363d6e34f3b6d43174a587684e
R1nge <r1ngegamedev@gmail.com>
2025-03-06 15:08:29 +0300

    detected gender

--- detect2.py
+++ detect2.py
@@ -2,6 +2,10 @@ from deepface import DeepFace
 import json
 import os
 
+sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
+statePath = os.path.join(sharedPath, "STATE")
+statePathJson = os.path.join(statePath, "STATE.json")
+
 backends = [
   'opencv', 
   'ssd', 
@@ -31,16 +35,23 @@ for file in os.listdir(sharedPath):
         objs = DeepFace.analyze(
             img_path = f"{sharedPath}\{file}", 
             #actions = ['age', 'gender', 'race', 'emotion']
-            actions = ['emotion']
+            actions = ['emotion', 'gender']
         )
         print(f"Prediction for {file}: {objs}")
-        data = objs[0]['dominant_emotion']
+        data = {
+            "emotion": objs[0]['dominant_emotion'],
+            "gender": objs[0]['dominant_gender']
+        }
         print(data)
         with open(os.path.join(sharedPath, f"{i}_face.json"), 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         i += 1
 
 
-
+state = {
+            "state": "detection_finished"
+        }
+with open(statePathJson,'w', encoding='utf-8') as f:
+    json.dump(state, f,ensure_ascii=False, indent=4)
 
 print(objs)
